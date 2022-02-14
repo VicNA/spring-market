025c50b13294f3b34673674897bc5995bef7a013
VicN <vnikolaeff989@gmail.com>
2022-02-14 16:30:33 +0300

    Add JwtTokenUtil

--- src/main/java/ru/geekbrains/spring/market/controllers/AuthController.java
+++ src/main/java/ru/geekbrains/spring/market/controllers/AuthController.java
@@ -3,10 +3,12 @@ package ru.geekbrains.spring.market.controllers;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 import ru.geekbrains.spring.market.services.UserService;
+import ru.geekbrains.spring.market.utils.JwtTokenUtil;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
 
     private final UserService userService;
+    private final JwtTokenUtil jwtTokenUtil;
 }
