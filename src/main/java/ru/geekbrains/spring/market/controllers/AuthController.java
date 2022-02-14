19949ed167fd773a41bd105dfb303d4da270169e
VicN <vnikolaeff989@gmail.com>
2022-02-14 16:44:35 +0300

    Add AuthController

--- src/main/java/ru/geekbrains/spring/market/controllers/AuthController.java
+++ src/main/java/ru/geekbrains/spring/market/controllers/AuthController.java
@@ -1,7 +1,16 @@
 package ru.geekbrains.spring.market.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.geekbrains.spring.market.dtos.JwtRequest;
+import ru.geekbrains.spring.market.dtos.JwtResponse;
 import ru.geekbrains.spring.market.services.UserService;
 import ru.geekbrains.spring.market.utils.JwtTokenUtil;
 
@@ -11,4 +20,19 @@ public class AuthController {
 
     private final UserService userService;
     private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public ResponseEntity<?> createAuthToken(@RequestBody JwtRequest authRequest) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            authRequest.getUsername(),
+                            authRequest.getPassword()));
+        } catch (BadCredentialsException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
+        String token = jwtTokenUtil.generateToken(userDetails);
+        return ResponseEntity.ok(new JwtResponse(token));
+    }
 }
