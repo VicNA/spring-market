bf8f36b15ff681a5b189a8f6b20d5bdf9e93625f
viktor-n <vnikolaeff989@gmail.com>
2022-03-05 12:09:59 +0300

    Приведение классов к пятому уроку: модуль api

---
+++ api/src/main/java/ru/geekbrains/spring/api/JwtResponse.java
@@ -0,0 +1,22 @@
+package ru.geekbrains.spring.api;
+
+public class JwtResponse {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+
+    public JwtResponse() {
+    }
+
+    public JwtResponse(String token) {
+        this.token = token;
+    }
+}
---
+++ api/src/main/java/ru/geekbrains/spring/api/StringResponse.java
@@ -0,0 +1,23 @@
+package ru.geekbrains.spring.api;
+
+public class StringResponse {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+
+
+    public StringResponse() {
+    }
+
+    public StringResponse(String value) {
+        this.value = value;
+    }
+}
