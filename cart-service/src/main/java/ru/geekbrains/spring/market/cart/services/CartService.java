f40ff13e9fc4b8d6e078f4986553fe657ed8504e
VicN <vnikolaeff989@gmail.com>
2022-04-05 12:25:40 +0300

    Домашняя работа к десятому уроку: рефакторинг модуля cart

--- cart-service/src/main/java/ru/geekbrains/spring/market/cart/services/CartService.java
+++ cart-service/src/main/java/ru/geekbrains/spring/market/cart/services/CartService.java
@@ -23,34 +23,44 @@ public class CartService {
     @Value("${cart-service.cart-prefix}")
     private String cartPrefix;
 
-    public Cart getCurrentCart(String uuid) {
-        String targetUuid = cartPrefix + uuid;
+    public Cart getCurrentCart(String username, String uuid) {
+        return getCurrentCart(getCartUuid(username, uuid));
+    }
+
+    public Cart getCurrentCart(String targetUuid) {
         if (!redisTemplate.hasKey(targetUuid)) {
             redisTemplate.opsForValue().set(targetUuid, new Cart());
         }
         return (Cart) redisTemplate.opsForValue().get(targetUuid);
     }
 
-    public void add(String uuid, Long productId) {
+    public void add(String username, String uuid, Long productId) {
         ProductDto product = productServiceIntegration.getProductById(productId);
-        execute(uuid, cart -> cart.add(product));
+        execute(getCartUuid(username, uuid), cart -> cart.add(product));
     }
 
-    public void remove(String uuid, Long productId) {
-        execute(uuid, cart -> cart.remove(productId));
+    public void remove(String username, String uuid, Long productId) {
+        execute(getCartUuid(username, uuid), cart -> cart.remove(productId));
     }
 
-    public void exclude(String uuid, Long productId) {
-        execute(uuid, cart -> cart.exclude(productId));
+    public void exclude(String username, String uuid, Long productId) {
+        execute(getCartUuid(username, uuid), cart -> cart.exclude(productId));
     }
 
-    public void clear(String uuid) {
-        execute(uuid, Cart::clear);
+    public void clear(String username, String uuid) {
+        execute(getCartUuid(username, uuid), Cart::clear);
     }
 
-    private void execute(String uuid, Consumer<Cart> operation) {
-        Cart cart = getCurrentCart(uuid);
+    private void execute(String targetUuid, Consumer<Cart> operation) {
+        Cart cart = getCurrentCart(targetUuid);
         operation.accept(cart);
-        redisTemplate.opsForValue().set(cartPrefix + uuid, cart);
+        redisTemplate.opsForValue().set(targetUuid, cart);
+    }
+
+    private String getCartUuid(String username, String uuid) {
+        if (username != null) {
+            return cartPrefix + username;
+        }
+        return cartPrefix + uuid;
     }
 }
